1f962339a72e1e20cb7bdbc9e65a804ed9bfd5b2
Ananya <ananya200696@gmail.com>
2026-01-02 18:13:04 +0530

    dotenv package impplemented

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,19 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
-
+// import { ConfigService } from '@nestjs/config';
+import 'dotenv/config';
 // This instance loads .env files from the root by default
-const configService = new ConfigService();
-
+// const configService = new ConfigService();
+console.log('Database Configurations:');
+console.log('DB_HOST:', process.env['DB_HOST']);
+console.log('DB_PORT:', process.env['DB_PORT']);
+console.log('DB_USER:', process.env['DB_USER']);
+console.log('DB_NAME:', process.env['DB_NAME']);
 export default new DataSource({
   type: 'mssql',
   schema: 'dbo',
-  host: configService.get<string>('DB_HOST') || 'localhost',
-  port: configService.get<number>('DB_PORT') || 1433,
-  username: configService.get<string>('DB_USER') || 'ananyalogin',
-  password: configService.get<string>('DB_PASSWORD') || '123',
-  database: configService.get<string>('DB_NAME') || 'EliteDB',
+  host: process.env['DB_HOST'] || 'localhost',
+  port: parseInt(process.env['DB_PORT'] as string) || 1433,
+  username: process.env['DB_USER'] || 'ananyalogin',
+  password: process.env['DB_PASSWORD'] || '123',
+  database: process.env['DB_NAME'] || 'EliteDB',
   //   autoLoadEntities: true,
   options: {
     trustServerCertificate: true,
